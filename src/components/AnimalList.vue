3f505be88893da770510bee81b39b1d14cd0b0ff
Duler <dusanradovic76@gmail.com>
2019-11-19 21:12:12 +0100

    zadatak 2

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -2,19 +2,20 @@
 <template>
   <div :id="animals" >
   <table style="width:100%">
-  <tr>
-    <th>Species</th>
-    <th>Name</th>
-    <th>Date of birth</th>
-  </tr>
-  <tr v-for="(animal, index) in animals" :key="index">
-    <td>{{animal.species}}</td>
-    <td>{{animal.name}}</td>
-    <td>{{animal.dateOfBirth}}</td>
-  </tr>
+    <tr>
+      <th>Species</th>
+      <th>Name</th>
+      <th>Date of birth</th>
+    </tr>
+    <tr v-for="(animal, index) in animals" :key="index">
+      <td>{{animal.species}}</td>
+      <td>{{animal.name}}</td>
+      <td>{{animal.dateOfBirth}}</td>
+      <td>{{animal.dateOfBirth === '' ? 'Unknown' : animal.dateOfBirth}}</td>
+    </tr>
   
   
-</table> 
+  </table> 
   </div>
 </template>
 <script>
@@ -23,9 +24,9 @@ export default {
     return {
       animals:
       [
-        {species: 'Hawk', name: 'Sokostark', dateOfBirth: '12.12.1992'},
-        {species: 'Lion', name: 'Leo', dateOfBirth: '22.08.1990'},
+        {species: 'Hawk', name: 'SokoStark', dateOfBirth: '12.12.1992'},
         {species: 'Fish', name: 'Vanda', dateOfBirth: '12.05.2016'},
+        {species: 'Lion', name: 'Leo', dateOfBirth: '22.08.1990'},        
         {species: 'Dog', name: 'Zuca', dateOfBirth: ''},
         {species: 'Parrot', name: 'Ara', dateOfBirth: '25.01.2010'}
       ]
