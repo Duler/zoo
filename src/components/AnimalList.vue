d4e9ee709781bc24f42931c0efb6c934804ab347
Duler <dusanradovic76@gmail.com>
2019-11-19 20:57:47 +0100

    zadatak1

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,19 +1,34 @@
+
 <template>
-  <div :id="msg"  >
-    
+  <div :id="animals" >
+  <table style="width:100%">
+  <tr>
+    <th>Species</th>
+    <th>Name</th>
+    <th>Date of birth</th>
+  </tr>
+  <tr v-for="(animal, index) in animals" :key="index">
+    <td>{{animal.species}}</td>
+    <td>{{animal.name}}</td>
+    <td>{{animal.dateOfBirth}}</td>
+  </tr>
+  
+  
+</table> 
   </div>
 </template>
-
 <script>
-import moment from 'moment'
-
 export default {
   data() {
     return {
-      name: 'AnimalList',
-      props: {
-      msg: "Zoo"
-      }
+      animals:
+      [
+        {species: 'Hawk', name: 'Sokostark', dateOfBirth: '12.12.1992'},
+        {species: 'Lion', name: 'Leo', dateOfBirth: '22.08.1990'},
+        {species: 'Fish', name: 'Vanda', dateOfBirth: '12.05.2016'},
+        {species: 'Dog', name: 'Zuca', dateOfBirth: ''},
+        {species: 'Parrot', name: 'Ara', dateOfBirth: '25.01.2010'}
+      ]
     }
   },
   methods: {
@@ -22,9 +37,9 @@ export default {
   
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-
+table, th, td {
+  border: 1px solid black;
+}
 </style>
