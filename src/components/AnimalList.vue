66a7ad3b1acfbb97870dfe292aa0d465cd7d4e00
Duler <dusanradovic76@gmail.com>
2019-11-19 19:29:09 +0100

    nesto

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :id="msg"  >
+    
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  data() {
+    return {
+      name: 'AnimalList',
+      props: {
+      msg: "Zoo"
+      }
+    }
+  },
+  methods: {
+      
+    },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
